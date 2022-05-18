f2407a5255602848e13331cf91ff7f5562b4fa74
mixed_nuts_nuts <88083304+mixnuts07@users.noreply.github.com>
2022-05-18 18:10:36 +0900

    Create Chapter3.ts

---
+++ Chapter3.ts
@@ -0,0 +1,11 @@
+// ts .. 純粋なts
+// tsx .. jsx構文を含む 
+// DOM .. HTMLをJSから参照操作する仕組み。→ HTMLを文字列ではなく、オブジェクトとして処理ができる。　HTMLを操作するAPIの様なもの。
+// 仮想DOM .. DOMのプロキシの様なもの。
+
+function increment(num:number){
+  return num + 1;
+}
+
+console.log(increment(9999};
+
